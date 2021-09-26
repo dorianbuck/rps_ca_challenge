a4574b66aa90d37484c15c5eff2cc6d7869e824b
Dorian Buck <dorianbuck@gmail.com>
2021-09-26 11:02:00 +0200

    adds grid to HandGesture component

--- src/components/HandGesters.jsx
+++ src/components/HandGesters.jsx
@@ -1,23 +1,35 @@
 import React from "react";
-import { Icon } from "semantic-ui-react";
+import { Icon, Header, Segment, Grid } from "semantic-ui-react";
 
 const HandGesters = () => {
   return (
-    <>
+    <Segment>
+      <Grid columns={2}>
+        <Grid.Column>
+      <Header as='h1' icon>
       <Icon
         data-cy="user-image"
         name="hand rock outline"
         size="massive"
         rotated="clockwise"
-      />
+        />
+        Player
+      </Header>
+        </Grid.Column>
+        <Grid.Column>
+      <Header as='h1' icon>
       <Icon
         data-cy="computer-image"
         name="hand rock outline"
         size="massive"
         rotated="counterclockwise"
         
-      />
-    </>
+        />
+        Computer
+        </Header>
+        </Grid.Column>
+      </Grid>
+    </Segment>
   );
 };
 
