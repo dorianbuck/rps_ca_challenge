74955a7895d0784d30fd662175dc65408745e7b2
Dorian Buck <dorianbuck@gmail.com>
2021-09-27 00:03:18 +0200

    Adds sinon dep and custom command in cy in hopes to fix MAth.random stub

--- cypress/integration/userCanWinOrLoseByGameLogic.feature.spec.js
+++ cypress/integration/userCanWinOrLoseByGameLogic.feature.spec.js
@@ -3,34 +3,30 @@ describe("The game logic is being tested", () => {
     beforeEach(() => {
       cy.visit("/"),
         {
-          onBeforeLoad(Math) {
-            cy.stub(Math, "random").returns('Scissor');
+          onBeforeLoad(win) {
+            cy.stub(win.Math, "randomChoice").returns('Scissor');
           },
         };
-      cy.get("[data-cy=btn-group]").as("btnGroup");
+        cy.get("[data-cy=btn-group]").as("btnGroup");
     });
     it("is expected to display the computer choice and outcome when users choice is rock", () => {
-      
-      onBeforeLoad(Math)
-        cy.stub(Math, "random").returns('Scissor');
-      
+      cy.visit("/", {
+        onBeforeLoad(win) {
+          cy.stub(win.Math, "random").returns('Scissor');
+        },
+      });
       cy.get("@btnGroup").first().click();
       cy.get("[data-cy=computer-choice-info]").should("contain", "Scissor");
       cy.get("[data-cy=game-results]").should("contain", "YOU WIN!");
     });
     it("is expected to display the computer choice and outcome when users choice is paper", () => {
-      cy.get("@btnGroup")
-      .first()
-      .next()
-      .click();
-    cy.get("button.ui.button").should("contain", "Paper")
+      cy.get("@btnGroup").first().next().click();
+      cy.get("button.ui.button").should("contain", "Paper");
       cy.get("[data-cy=game-results]").should("contain", "YOU LOST!");
     });
     it("is expected to display the computer choice and outcome when users choice is scissor", () => {
-      cy.get("@btnGroup")
-      .last()
-      .click();
-    cy.get("button.ui.button").should("contain", "Scissor");
+      cy.get("@btnGroup").last().click();
+      cy.get("button.ui.button").should("contain", "Scissor");
       cy.get("[data-cy=game-results]").should("contain", "ITS A DRAW!");
     });
   });
