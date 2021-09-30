39e38f86b2fa7967f8c012d3c44b1d7f4000e91b
Dorian Buck <dorianbuck@gmail.com>
2021-09-30 14:59:21 +0200

    adds index for btn and changed test to make them passs

--- cypress/integration/userCanWinOrLoseByGameLogic.feature.spec.js
+++ cypress/integration/userCanWinOrLoseByGameLogic.feature.spec.js
@@ -6,20 +6,19 @@ describe("The game logic is being tested", () => {
           cy.stub(win.Math, "random").returns("0");
         },
       });
-      cy.get("[data-cy=btn-group]").as("btnGroup");
     });
     it("is expected to display the computer choice and outcome when users choice is rock", () => {
-      cy.get("@btnGroup").first().click();
+      cy.get("[data-cy=btn-0]").click();
       cy.get("[data-cy=computer-choice-info]").should("contain", "Rock");
       cy.get("[data-cy=game-results]").should("contain", "ITS A DRAW!");
     });
     it("is expected to display the computer choice and outcome when users choice is paper", () => {
-      cy.get("@btnGroup").first().next().click();
+      cy.get("[data-cy=btn-1]").click();
       cy.get("button.ui.button").should("contain", "Rock");
       cy.get("[data-cy=game-results]").should("contain", "YOU WIN!");
     });
     it("is expected to display the computer choice and outcome when users choice is scissor", () => {
-      cy.get("@btnGroup").last().click();
+      cy.get("[data-cy=btn-2]").click();
       cy.get("button.ui.button").should("contain", "Rock");
       cy.get("[data-cy=game-results]").should("contain", "YOU LOST!");
     });
